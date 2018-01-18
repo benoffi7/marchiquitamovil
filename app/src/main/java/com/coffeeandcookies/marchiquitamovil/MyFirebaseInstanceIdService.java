5df57cc396107fdf3061b120f315a74caa354e4c
Gonzalo Benoffi <benoffi11@gmail.com>
2018-01-18 18:44:01 -0300

    Creación del proyecto. Añadido Firebase. Añadido Mapas.

---
+++ app/src/main/java/com/coffeeandcookies/marchiquitamovil/MyFirebaseInstanceIdService.java
@@ -0,0 +1,25 @@
+package com.coffeeandcookies.marchiquitamovil;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+
+/**
+ * Created by Gonzalo on 18/01/2018.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService
+{
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("gcm", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+    }
+}
